5822c2c20f7bb34493a6977b4ac0e6f7a5e2f934
ivanbarbona7 <95719345+ivanbarbona7@users.noreply.github.com>
2026-08-05 11:21:33 -0300

    Delete src/routes/HistoriaClinicas.routes.js

--- src/routes/HistoriaClinicas.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getHistoriaClinicas, createHistoriaClinica, deleteHistoriaClinica } = require('../controllers/HistoriaClinica.controller'); 
-
-router.get('/', getHistoriasClinicas);
-router.post('/', createHistoriaClinica);
-router.delete('/:id', deleteHistoriaClinica);
-
-module.exports = router;
